0a698aa2e5221078e1aba08df97301b20200ee95
Ryo Shimizu <ryo@nerv.org>
2019-11-20 05:10:25 +0900

    fix typ

--- libl2pkt/utils.c
+++ libl2pkt/utils.c
@@ -79,6 +79,6 @@ packetdump(const char *packet, size_t pktsize)
 
 	eh = (struct ether_header *)packet;
 	strncpy(buf, ether_ntoa((struct ether_addr *)eh->ether_shost), sizeof(buf));
-	printf("%s -> %s, ethertyp 0x%04x\n", buf, ether_ntoa((struct ether_addr *)eh->ether_dhost), ntohs(eh->ether_type));
+	printf("%s -> %s, ethertype 0x%04x\n", buf, ether_ntoa((struct ether_addr *)eh->ether_dhost), ntohs(eh->ether_type));
 	dumpstr(packet + sizeof(struct ether_header), pktsize - sizeof(struct ether_header));
 }
